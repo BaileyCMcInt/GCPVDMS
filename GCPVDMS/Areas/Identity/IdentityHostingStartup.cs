edc3be64e345db17a0720558d6e6f412ccdb9ad8
Draefuglestad <draefuglestad@yahoo.com>
2020-09-25 17:20:30 -0400

    Added ASP.Net Identity Package

---
+++ GCPVDMS/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GCPVDMS.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GCPVDMS.Areas.Identity.IdentityHostingStartup))]
+namespace GCPVDMS.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<GCPVDMSContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("GCPVDMSContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<GCPVDMSContext>();
+            });
+        }
+    }
+}
