03e3dd779c507ff5e21562a5524a949af658bded
Draefuglestad <draefuglestad@yahoo.com>
2020-09-30 14:39:34 -0400

    Added more custom data fields to the user table

--- GCPVDMS/Models/ApplicationUser.cs
+++ GCPVDMS/Models/ApplicationUser.cs
@@ -7,7 +7,10 @@ using System.Threading.Tasks;
 namespace GCPVDMS.Models
 {
     public class ApplicationUser : IdentityUser
-    {   public string FirstName { get; set; }
+    { public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string LocationPreference {get; set; }
+        public string Availablity { get; set; }
     }
+
 }
