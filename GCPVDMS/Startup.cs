5c622abe4459cc2036690e3cc5d8bb0acc32e514
Draefuglestad <draefuglestad@yahoo.com>
2020-10-21 13:29:50 -0400

    Changes made with Greg regarding Azure

--- GCPVDMS/Startup.cs
+++ GCPVDMS/Startup.cs
@@ -27,7 +27,7 @@ namespace GCPVDMS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<GCPVDMSContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<GCPVDMSContext>(options => options.UseSqlServer(Configuration.GetConnectionString("GCPVDMSContextConnection")));
             services.AddIdentity<ApplicationUser, IdentityRole>()
                     .AddEntityFrameworkStores<GCPVDMSContext>()
                     .AddDefaultUI()
