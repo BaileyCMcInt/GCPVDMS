9adb60931d942bc842eece3e00778fa8a11e1aa2
Draefuglestad <draefuglestad@yahoo.com>
2020-10-28 00:24:00 -0400

    Add role functionality

---
+++ GCPVDMS/Models/RoleEdit.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace GCPVDMS.Models
+{
+    public class RoleEdit
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+    }
+}
+//The RoleEdit class is used to represent the Role and the details of the Users who are in the role or not in the role.
---
+++ GCPVDMS/Models/RoleModification.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GCPVDMS.Models
+{
+    public class RoleModification
+    {
+        [Required]
+        public string RoleName { get; set; }
+
+        public string RoleId { get; set; }
+
+        public string[] AddIds { get; set; }
+
+        public string[] DeleteIds { get; set; }
+    }
+}
+//The RoleModification class code will help in doing the changes to a role.
