56964944c69dc348e89b1822f2ab0d3d885e6771
Bailey <bcmcint@gmail.com>
2020-10-29 10:19:59 -0400

    Added General Event info page

---
+++ GCPVDMS/Views/Event/EventSignUp.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GCPVDMS.Views.Event
+{
+    public class EventSignUpModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
