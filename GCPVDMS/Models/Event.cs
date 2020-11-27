21169e77a6c967fdfe0716f7a279d12082b4339e
Rach <rachmarie1031@gmail.com>
2020-11-27 00:12:01 -0500

    Created Event model and added migration.

---
+++ GCPVDMS/Models/Event.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCPVDMS.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+        public DateTime EventDate { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+        public string EventTitle { get; set; }
+
+        public string EventDescription { get; set; }
+
+        public string POCName {get; set; }
+
+        public string POCPhone { get; set; }
+
+        public string POCEmail { get; set; }
+
+        public bool isEventActive { get; set; }
+
+        public int NumVolunteersNeeded { get; set; }
+
+        public int NumVolunteersSignedUp { get; set; }
+
+        [ForeignKey("LocationID")]
+        public int LocationID { get; set; }
+        public Location Location { get; set; }
+    }
+}
