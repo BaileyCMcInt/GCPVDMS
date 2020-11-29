082acc8b4176823b59c1efdc8897fbd9c8be07f5
Draefuglestad <draefuglestad@yahoo.com>
2020-11-28 22:30:21 -0500

    tesst

--- GCPVDMS/Controllers/EventController.cs
+++ GCPVDMS/Controllers/EventController.cs
@@ -10,10 +10,10 @@ namespace GCPVDMS.Controllers
     public class EventController : Controller
     {
 
-        public IActionResult Index()
-        {
-            return View("~/Views/Event/Admin/Index.cshtml");
-        }
+        //public IActionResult Index()
+        //{
+        //    return View("~/Views/Event/Admin/Index.cshtml");
+        //}
 
         //public IActionResult EventList()
         //{
@@ -32,9 +32,9 @@ namespace GCPVDMS.Controllers
         }
         public ViewResult EventList() => View("~/Views/Event/Admin/EventList.cshtml", repository.Events);
 
-        //public ViewResult Index(int eventId) =>
-        //    View("~/Views/Event/Admin/Index.cshtml",repository.Events
-        //    .FirstOrDefault(p => p.EventID == eventId));
+        public ViewResult Index(int eventId) =>
+            View("~/Views/Event/Admin/Index.cshtml", repository.Events
+            .FirstOrDefault(p => p.EventID == eventId));
 
         [HttpPost]
         public IActionResult Index(Event @event)
