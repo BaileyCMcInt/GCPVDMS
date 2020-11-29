7c4ea4017fe907659b90b50345b2baa6b275e462
Draefuglestad <draefuglestad@yahoo.com>
2020-11-29 12:10:43 -0500

    test

--- GCPVDMS/Program.cs
+++ GCPVDMS/Program.cs
@@ -19,13 +19,7 @@ namespace GCPVDMS
         public static void Main(string[] args)
         {
            CreateHostBuilder(args).Build().Run();
-            var newbuilder = new ConfigurationBuilder()
-             .SetBasePath(Directory.GetCurrentDirectory())
-             .AddJsonFile("appsettings.json");
-
-            IConfiguration iconfig = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", true, true)
-                .Build();
+          
 
         }
 
