f1e30c7c230e2b68f3f2a816c1b2f4b501b1da6b
Draefuglestad <draefuglestad@yahoo.com>
2020-12-01 15:15:57 -0500

    connection string change

--- GCPVDMS/Startup.cs
+++ GCPVDMS/Startup.cs
@@ -32,10 +32,10 @@ namespace GCPVDMS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var x = Configuration.GetConnectionString("GCPVDMSContextConnection");
-            services.AddDbContext<GCPVDMSContext>(options => options.UseSqlServer(x));
-            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(x));
-           
+            //var x = Configuration.GetConnectionString("GCPVDMSContextConnection");
+            services.AddDbContext<GCPVDMSContext>(options => options.UseSqlServer(Configuration.GetConnectionString("GCPVDMSContextConnection")));
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("GCPVDMSContextConnection")));
+
             services.AddTransient<IEventRepository, EFEventRepository>();
             //services.AddTransient<IDriveRepository, EFDriveRepository>(); //added missing package that enables these to interact with connection string
 
