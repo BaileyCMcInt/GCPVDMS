dfcdc7676eb216c51ad8ba7429442884a84632a8
Draefuglestad <draefuglestad@yahoo.com>
2020-12-07 19:53:24 -0500

    Commenting out methods from event controller no longer used, making requested role page updates., adding linkedin button and changing nav bar link colors to be more noticeable

--- GCPVDMS/Controllers/EventController.cs
+++ GCPVDMS/Controllers/EventController.cs
@@ -10,21 +10,14 @@ namespace GCPVDMS.Controllers
 {
     public class EventController : Controller
     {
-
-        //public IActionResult Index()
-        //{
-        //    return View("~/Views/Event/Admin/Index.cshtml");
-        //}
-
-        //public IActionResult EventList()
-        //{
-        //    return View("~/Views/Event/Admin/EventList.cshtml");
-        //}
-
         private IEventRepository repository;
         public EventController(IEventRepository repo)
         {
             repository = repo;
+        }
+        public IActionResult Index()
+        {
+            return RedirectToAction("EventInfoPage");
         }
         public ViewResult EventSignUp() =>
             View("~/Views/Event/Volunteer/EventSignUp.cshtml", repository.Events);
@@ -38,37 +31,37 @@ namespace GCPVDMS.Controllers
 
         public ViewResult HostaDrive() => View("~/Views/Event/Volunteer/HostaDrive.cshtml");
 
-        [Authorize(Roles = "Global Admin")]
-        public ViewResult EventList() => View("~/Views/Event/Admin/EventList.cshtml", repository.Events);
+        //[Authorize(Roles = "Global Admin")]
+        //public ViewResult EventList() => View("~/Views/Event/Admin/EventList.cshtml", repository.Events);
 
-        [Authorize(Roles = "Global Admin")]
-        public ViewResult Index(int eventId) =>
-            View("~/Views/Event/Admin/Index.cshtml", repository.Events
-                .FirstOrDefault(p => p.EventID == eventId));
+        //[Authorize(Roles = "Global Admin")]
+        //public ViewResult Index(int eventId) =>
+        //    View("~/Views/Event/Admin/Index.cshtml", repository.Events
+        //        .FirstOrDefault(p => p.EventID == eventId));
 
-        [Authorize(Roles = "Global Admin")]
-        public ViewResult DisplayEvent(int eventId) =>
-         View("~/Views/Event/Admin/EventInfo.cshtml", repository.Events
-          .FirstOrDefault(p => p.EventID == eventId));
+        //[Authorize(Roles = "Global Admin")]
+        //public ViewResult DisplayEvent(int eventId) =>
+        // View("~/Views/Event/Admin/EventInfo.cshtml", repository.Events
+        //  .FirstOrDefault(p => p.EventID == eventId));
 
-        [Authorize(Roles = "Global Admin")]
-        [HttpPost]
-        public IActionResult Index(Event @event)
-        {
-            if (ModelState.IsValid)
-            {
-                repository.SaveEvent(@event);
-             //   TempData["message"] = $"{event.EventTitle} has been saved";
-                return RedirectToAction("EventList");
-            }
-            else
-            {
-                // there is something wrong with the data values
-                return View(@event);
-            }
-        }
+        //[Authorize(Roles = "Global Admin")]
+        //[HttpPost]
+        //public IActionResult Index(Event @event)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        repository.SaveEvent(@event);
+        //     //   TempData["message"] = $"{event.EventTitle} has been saved";
+        //        return RedirectToAction("EventList");
+        //    }
+        //    else
+        //    {
+        //        // there is something wrong with the data values
+        //        return View(@event);
+        //    }
+        //}
 
-        [Authorize(Roles = "Global Admin")]
-        public ViewResult Create() => View("~/Views/Event/Admin/Index.cshtml", new Event());
+        //[Authorize(Roles = "Global Admin")]
+        //public ViewResult Create() => View("~/Views/Event/Admin/Index.cshtml", new Event());
     }
 }
