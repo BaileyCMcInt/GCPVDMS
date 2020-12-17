e49847d361d26d202788495e76a2747b1e046601
Rach <rachmarie1031@gmail.com>
2020-12-17 15:06:45 -0500

    Added GCPTask.cs and GCPEventTask.cs models. Migrated and updated database.

---
+++ GCPVDMS/Models/GCPEventTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace GCPVDMS.Models
+{
+    public class GCPEventTask
+    {
+        public int GCPEventTaskID { get; set; }
+
+        [ForeignKey("EventID")]
+        public int EventID { get; set; }
+        public Event Event { get; set; }
+
+        [ForeignKey("TaskID")]
+        public int GCPTaskID { get; set; }
+        public GCPTask GCPTask { get; set; }
+    }
+}
---
+++ GCPVDMS/Models/GCPTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GCPVDMS.Models
+{
+    public class GCPTask
+    {
+        public int GCPTaskID { get; set; }
+
+        public string TaskName { get; set; }
+    }
+}
