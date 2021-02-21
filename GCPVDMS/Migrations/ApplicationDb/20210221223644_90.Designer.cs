c377ea68cca4a787ca673ca8f529fafb4321285b
Draefuglestad <draefuglestad@yahoo.com>
2021-02-21 18:00:26 -0500

    Added volunteer/donor dash option to admin account. Added isSelected attribute.

--- GCPVDMS/Migrations/ApplicationDb/20210221223644_90.Designer.cs
+++ GCPVDMS/Migrations/ApplicationDb/20210221223644_90.Designer.cs
@@ -4,14 +4,16 @@ using GCPVDMS.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GCPVDMS.Migrations.ApplicationDb
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210221223644_90")]
+    partial class _90
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GCPVDMS/Migrations/ApplicationDb/20210221223644_90.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GCPVDMS.Migrations.ApplicationDb
+{
+    public partial class _90 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isSelected",
+                table: "GCPEventTasks",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isSelected",
+                table: "GCPEventTasks");
+        }
+    }
+}
